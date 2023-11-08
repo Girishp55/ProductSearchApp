ddc0c4092789d25fb01881032704288bacc3b094
Dell <Dell@DESKTOP-OF3FMTE>
2023-11-08 19:44:23 +0530

    Add project files.

---
+++ ProductSearchApp/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using ProductSearchApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductSearchApp.Controllers
+{
+    public class ProductController : Controller
+    {
+       girishEntities db = new girishEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.tblProducts.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searching)
+        {
+            return View(db.searchproduct(searching).ToList());
+        }
+
+    }
+}
